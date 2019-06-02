7893dbdb5789e5fbc7d9e4c500374a425b32196d
Rasoul <39953546+rasoulkajouri@users.noreply.github.com>
2019-06-02 19:15:06 +0430

    Add files via upload

---
+++ recursive_summation.cpp
@@ -0,0 +1,41 @@
+/* ##################################################################
+   The code finds the sum of all elements of a sequence by the
+   recursive method.
+
+   This code has written by Rasoul Kajouri.
+   for more info. rasoulkajouri@gmail.com
+
+   Copy lefts, all lefts reserved! Last update : Jun 2 2019
+
+   ################################################################*/
+
+#include <iostream>
+#include <vector>
+
+int sum_list (std::vector <int> list, int l){
+
+	int dummy = list[l];
+	list.pop_back();
+
+	if(l == 0)
+		return dummy;
+
+	return dummy + sum_list(list, l-1);
+}
+
+int main(){
+
+	std::cout << " Enter your numbers, at the end entry character \'n\'.\n";
+
+	int x, i=0;
+	std::vector<int> a;
+
+	while(std::cin >> x){
+		a.push_back(x);
+		++i;
+	}
+
+	std::cout << " Sum of all elements are " << sum_list(a, i-1) << " .\n";
+
+	return 0;
+}
